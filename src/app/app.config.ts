0af88c1b8e1baf644a3bb9615e451d62e156ad91
hendersonkleber <hendersonkleber@gmail.com>
2025-07-25 14:19:54 -0300

    Inicializado projeto

---
+++ src/app/app.config.ts
@@ -0,0 +1,22 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+  withIncrementalHydration,
+} from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideClientHydration(withIncrementalHydration(), withEventReplay()),
+    provideRouter(routes),
+  ],
+};
